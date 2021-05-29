bb4632d2665215a200b545c50692ec6a2ae2db4e
rizerphe <bohdanopyr@gmail.com>
2021-05-29 09:36:14 +0300

    Renamed some variables to save space
    I'd prefer the final version to be less than 2000 characters long

--- argh.py
+++ argh.py
@@ -1,4 +1,4 @@
-try_ = lambda t, f=lambda a:a, e=Exception,:(r:={}).pop(
+t = lambda t, f=lambda a:a, e=Exception,:(r:={}).pop(
     'r',
     type(
         '',
@@ -13,15 +13,15 @@ try_ = lambda t, f=lambda a:a, e=Exception,:(r:={}).pop(
         }
     )()(t)()
 )# Inline error handling, credit: chilaxan from python discord; modified version
-def argh(code):
-    (ptr := [0,0])
-    (dr := [0])
-    (sck := [])
-    (field := [list(l)+[" "]*(80-len(l)) for l in code.split("\n")]+[["  "]*80]*(40-len(code.split("\n"))))
-    (ss:=sck.__setitem__)
-    (x:=lambda a:ptr.__setitem__(1,a))
-    (y:=lambda a:ptr.__setitem__(0,a))
-    list(iter(lambda: [{"A":lambda:ss(-1,sck[-1]+ord(field[ptr[0]-1][ptr[1]])),"a":lambda:ss(-1,sck[-1]+ord(field[ptr[0]+1][ptr[1]])),"D":lambda:sck.pop(),"d":lambda:sck.append(sck[-1]),"E":lambda:field[ptr[0]-1].__setitem__(ptr[1],chr(26)),"e":lambda:field[ptr[0]+1].__setitem__(ptr[1],chr(26)),"F":lambda:field[ptr[0]-1].__setitem__(ptr[1],chr(sck.pop())),"f":lambda:field[ptr[0]+1].__setitem__(ptr[1],chr(sck.pop())),"G":lambda:try_(lambda: field[ptr[0]-1].__setitem__(ptr[1],input()[0]), lambda: field[ptr[0]-1].__setitem__(ptr[1],chr(26)), EOFError),"g":lambda:try_(lambda: field[ptr[0]+1].__setitem__(ptr[1],input()[0]), lambda: field[ptr[0]+1].__setitem__(ptr[1],chr(26)), EOFError),"H":lambda:[ptr.__setitem__(1,ptr[1]-list(reversed(field[ptr[0]][0:ptr[1]])).index(chr(sck[-1]))),dr.__setitem__(0,2)],"h":lambda:dr.__setitem__(0,2),"J":lambda:[y(ptr[0]+[l[0] for l in field[ptr[0]:]].index(chr(sck[-1]))),dr.__setitem__(0,1)],"j":lambda:dr.__setitem__(0,1),"K":lambda:[y(ptr[0]-list(reversed([l[0] for l in field[0:ptr[0]]])).index(chr(sck[-1]))),dr.__setitem__(0,3)],"k":lambda:dr.__setitem__(0,3),"L":lambda:[ptr.__setitem__(1,ptr[1]-field[ptr[0]][ptr[1]:].index(chr(sck[-1]))),dr.__setitem__(0,0)],"l":lambda:dr.__setitem__(0,0),"P":lambda:print(end=field[ptr[0]-1][ptr[1]]),"p":lambda:print(end=field[ptr[0]+1][ptr[1]]),"q":lambda:exit(),"R":lambda:ss(-1,sck[-1]-ord(field[ptr[0]-1][ptr[1]])),"r":lambda:ss(-1,sck[-1]-ord(field[ptr[0]+1][ptr[1]])),"S":lambda:sck.append(ord(field[ptr[0]-1][ptr[1]])),"s":lambda:sck.append(ord(field[ptr[0]+1][ptr[1]])),"X":lambda:dr.__setitem__(0,(dr[0]+3)%4) if sck[-1]<0 else None,"x":lambda:dr.__setitem__(0,(dr[0]+1)%4) if sck[-1]<0 else None,"#":lambda:dr.__setitem__(0,1) if ptr==[0,0] and field[0,1] == "!" else None}.get(field[ptr[0]][ptr[1]],lambda:[print("[ARGH!]"),exit()])(),[lambda:x(ptr[1]+1),lambda:y(ptr[0]+1),lambda:x(ptr[1]-1),lambda:y(ptr[0]-1)][dr[0]]()], None))
+def argh(c):
+    (p := [0,0])
+    (r := [0])
+    (k := [])
+    (field := [list(l)+[" "]*(80-len(l)) for l in c.split("\n")]+[["  "]*80]*(40-len(c.split("\n"))))
+    (s:=k.__setitem__)
+    (x:=lambda a:p.__setitem__(1,a))
+    (y:=lambda a:p.__setitem__(0,a))
+    list(iter(lambda: [{"A":lambda:s(-1,k[-1]+ord(field[p[0]-1][p[1]])),"a":lambda:s(-1,k[-1]+ord(field[p[0]+1][p[1]])),"D":lambda:k.pop(),"d":lambda:k.append(k[-1]),"E":lambda:field[p[0]-1].__setitem__(p[1],chr(26)),"e":lambda:field[p[0]+1].__setitem__(p[1],chr(26)),"F":lambda:field[p[0]-1].__setitem__(p[1],chr(k.pop())),"f":lambda:field[p[0]+1].__setitem__(p[1],chr(k.pop())),"G":lambda:t(lambda: field[p[0]-1].__setitem__(p[1],input()[0]), lambda: field[p[0]-1].__setitem__(p[1],chr(26)), EOFError),"g":lambda:t(lambda: field[p[0]+1].__setitem__(p[1],input()[0]), lambda: field[p[0]+1].__setitem__(p[1],chr(26)), EOFError),"H":lambda:[p.__setitem__(1,p[1]-list(reversed(field[p[0]][0:p[1]])).index(chr(k[-1]))),r.__setitem__(0,2)],"h":lambda:r.__setitem__(0,2),"J":lambda:[y(p[0]+[l[0] for l in field[p[0]:]].index(chr(k[-1]))),r.__setitem__(0,1)],"j":lambda:r.__setitem__(0,1),"K":lambda:[y(p[0]-list(reversed([l[0] for l in field[0:p[0]]])).index(chr(k[-1]))),r.__setitem__(0,3)],"k":lambda:r.__setitem__(0,3),"L":lambda:[p.__setitem__(1,p[1]-field[p[0]][p[1]:].index(chr(k[-1]))),r.__setitem__(0,0)],"l":lambda:r.__setitem__(0,0),"P":lambda:print(end=field[p[0]-1][p[1]]),"p":lambda:print(end=field[p[0]+1][p[1]]),"q":lambda:exit(),"R":lambda:s(-1,k[-1]-ord(field[p[0]-1][p[1]])),"r":lambda:s(-1,k[-1]-ord(field[p[0]+1][p[1]])),"S":lambda:k.append(ord(field[p[0]-1][p[1]])),"s":lambda:k.append(ord(field[p[0]+1][p[1]])),"X":lambda:r.__setitem__(0,(r[0]+3)%4) if k[-1]<0 else None,"x":lambda:r.__setitem__(0,(r[0]+1)%4) if k[-1]<0 else None,"#":lambda:r.__setitem__(0,1) if p==[0,0] and field[0,1] == "!" else None}.get(field[p[0]][p[1]],lambda:[print("[ARGH!]"),exit()])(),[lambda:x(p[1]+1),lambda:y(p[0]+1),lambda:x(p[1]-1),lambda:y(p[0]-1)][r[0]]()], None))
 
 argh('''j       world
 lppppppPPPPPPsrfj
